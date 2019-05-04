cd35c828ecb786c521944582b863356eed634bc1
raerae1205 <crashdean@hotmail.com>
2019-05-04 14:44:42 -0700

    Finished the functions and checked function

--- liri.js
+++ liri.js
@@ -1,42 +1,71 @@
-// require("dotenv").config();
-
+require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js")
 var axios = require("axios");
-
-
-
-
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 var commandQuery = process.argv[2];
-var search = process.argv.slice(3).join(" ");
-
+var artistMovieSearch = process.argv.slice(3).join(" ");
   switch (commandQuery) {
     case "concert-this":
-    console.log(search);
+    console.log(artistMovieSearch);
     bandsInTown();
       break;
       case "spotify-this-song":
-  what();
+    spotifyBands();
       break;
      case  "movie-this":
-    now();
+    movieSearch();
       break;
       case "do-what-it-says":
-    yes();
+      doWhatItSays();
       break;
-  
   }
 
-
-
-
-
 function bandsInTown() {
-axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
-  .then(function(response) {
-    
+axios.get("https://rest.bandsintown.com/artists/" + artistMovieSearch + "/events?app_id=codingbootcamp")
+  .then(function(response) {  
     console.log(response.data[0].venue.name);
     console.log(response.data[0].venue.city);
-    console.log(response.data[0].datetime);
-    
+    console.log(response.data[0].datetime);   
   })
+}
 
+function spotifyBands() {
+  spotify.search({ type: 'track', query: artistMovieSearch, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+}
+
+function movieSearch() {
+  axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
+    .then(function(response) {  
+      console.log(response.data);
+      // console.log(response.data[0].venue.city);
+      // console.log(response.data[0].datetime);   
+    })
+  }
+
+  function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    
+    if (error) {
+      return console.log(error);
+    }
+  
+   
+    console.log(data);
+  
+    
+    var dataArr = data.split(",");
+  
+    
+    console.log(dataArr);
+  
+  });
 }
