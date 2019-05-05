f8257df277191a2fe8c457fc44805c6ebea2b7c9
raerae1205 <crashdean@hotmail.com>
2019-05-04 22:44:39 -0700

    Uplaoding images

--- liri.js
+++ liri.js
@@ -32,59 +32,83 @@ function bandsInTown() {
 }
 
 function spotifyBands() {
-  if (artistMovieSearch === artistMovieSearch) {
+  if (artistMovieSearch) {
     spotify.search({ type: 'track', query: artistMovieSearch, limit: 1 }, function (err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
-
       }
+      console.log(data.tracks.artists);
+      console.log("The artist is: " + data.tracks.items[0].artists.type);
+      console.log("The song's name is: ", songData.album.artists[0].name);
+      console.log("Preview this song: " + data.tracks.items[0].preview_url);
+      console.log("The song is from the album: " + data.tracks.items[0].album.name);
+    }
+    )
+  }
+  else {
+    spotify.search({ type: 'track', query: "The Sign", limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("The artist is: " + data.tracks.items[0].artists.type);
+      console.log("The song's name is: ", songData.album.artists[0].name);
+      console.log("To preview song click: " + data.tracks.items[0].preview_url);
+      console.log("The song is from the album: " + data.tracks.items[0].album.name);
+    });
+  }
+}
+function movieSearch() {
+  if (artistMovieSearch) {
+    axios.get("http://www.omdbapi.com/?t=" + artistMovieSearch + "&y=&plot=short&apikey=trilogy")
+      .then(function (response) {
+        console.log(response.data);
+        console.log("The Ttitle of the movie is: " + response.data.Title);
+        console.log("The year the movie came out is: " + response.data.Year);
+        console.log("The rating of this movie is: " + response.data.Rated);
+        console.log("The year the movie came out is: " + response.data.Year);
+        console.log("The country were the movie was produced: " + response.data.Country);
+        console.log("The language of this movie is: " + response.data.Language);
+        console.log("The plot of this movie is: " + response.data.Plot);
+        console.log("The actors in this movie are: " + response.data.Actors);
+      })
+  }
+  else {
+    axios.get("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy")
+      .then(function (response) {
+        console.log(response.data);
+        console.log("The Ttitle of the movie is: " + response.data.Title);
+        console.log("The year the movie came out is: " + response.data.Year);
+        console.log("The rating of this movie is: " + response.data.Rated);
+        console.log("The year the movie came out is: " + response.data.Year);
+        console.log("The country were the movie was produced: " + response.data.Country);
+        console.log("The language of this movie is: " + response.data.Language);
+        console.log("The plot of this movie is: " + response.data.Plot);
+        console.log("The actors in this movie are: " + response.data.Actors);
+      })
+  }
+}
 
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    // console.log(data);
+    var dataArr = data.split(",");
+    // console.log(dataArr[1]);
+    var song = dataArr[1];
+    // console.log(song);
+    spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
       console.log("The artist is " + data.tracks.items[0].artists.type);
       // console.log("Artist Name: ", songData.album.artists[0].name);
       console.log("To preview song click " + data.tracks.items[0].preview_url);
       console.log("The song is from the album " + data.tracks.items[0].album.name);
     }
-    )}
-   
-   {
-        spotify.search({ type: 'track', query: "The Sign", limit: 1 }, function (err, data) {
-          if (err) {
-            return console.log('Error occurred: ' + err);
+    )
+  });
+}
 
-          }
-          console.log("The artist is " + data.tracks.items[0].artists.type);
-          // console.log("Artist Name: ", songData.album.artists[0].name);
-          console.log("To preview song click " + data.tracks.items[0].preview_url);
-          console.log("The song is from the album " + data.tracks.items[0].album.name);
 
-        });
-      }
-    }
-  
-function movieSearch() {
-        // if (artistMovieSearch === " ")
-        axios.get("http://www.omdbapi.com/?t=" + artistMovieSearch + "&y=&plot=short&apikey=trilogy")
-          .then(function (response) {
-            console.log(response.data);
-            console.log("The Ttitle of the movie is " + response.data.Title);
-            console.log("The year the movie came out is " + response.data.Year);
-            console.log("The rating of this movie is " + response.data.Rated);
-            console.log("The year the movie came out is " + response.data.Year);
-            console.log("The country were the movie was produced " + response.data.Country);
-            console.log("The language of this movie is " + response.data.Language);
-            console.log("The plot of this movie is " + response.data.Plot);
-            console.log("The actors in this movie are " + response.data.Actors);
-
-          })
-      }
-
-  function doWhatItSays() {
-        fs.readFile("random.txt", "utf8", function (error, data) {
-          if (error) {
-            return console.log(error);
-          }
-          console.log(data);
-          var dataArr = data.split(",");
-          console.log(dataArr);
-        });
-      }
